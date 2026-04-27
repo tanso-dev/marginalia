18c4c744fd3666d68079ddc8821f50f8830ac0a7
Miro Santos <tanso.dev@gmail.com>
2026-04-26 20:59:20 -0500

    Moves fonts up to the head section instead of using import to properly load fonts.

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;0,700;0,800;1,400;1,600&family=Source+Serif+4:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet" />
+      </head>
       <body>{children}</body>
     </html>
   );
